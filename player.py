dc7bf775f6dfb240267c4f7f5ee45e2b4bfd501e
MadsStilling <madsstilling@gmail.com>
2015-05-03 15:37:35 +0200

    Added methods to Player and modified .gitignore

--- player.py
+++ player.py
@@ -10,21 +10,40 @@ class Player(object):
         self.best_hand = None
 
     def high_card(self):
+        """Finds the highest cards in the hole.
+        """
         ranks = [rank.get_value() for rank in self.cards]
         if ranks[0] > ranks[1]:
             return ranks[0]
         else:
             return ranks[1]
 
-    def build_ranks(self):
-       pairs = {}
-       for item in self.combination:
-           item_count = 1
-           find_rank = item.get_rank()
-           if find_rank not in pairs:
-               pairs[find_rank] = item_count
+    def build_pairs(self):
+        """Creates a dictionary of the players card combination
+           sums the number of pairs into the dicts values.
+        """
+        pairs = {}
+        for item in self.combination:
+            item_count = 1
+            find_rank = item.get_rank()
+            if find_rank not in pairs:
+                pairs[find_rank] = item_count
+ 
+            else:
+                item_count += 1
+                pairs[find_rank]= item_count
+        return pairs
 
-           else:
-               item_count += 1
-               pairs[find_rank]= item_count
-       return pairs
+    def build_ranks(self):
+        """Creates a dictionary of the players card combination
+           sums the number of pairs into the dicts values.
+        """
+        sets = {}
+        for item in self.combination:
+            find_rank = item.get_rank()
+            if find_rank not in sets:
+               sets[find_rank] = [item]
+ 
+            else:
+                sets[find_rank].append([item])
+        return sets
